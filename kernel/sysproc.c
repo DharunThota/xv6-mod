62946e196e499e13db1a299acd1c8a4ada73dc95
Dharun Thota <dharunth@gmail.com>
2024-11-23 16:35:11 +0000

    add SIGINT

---
+++ kernel/sysproc.c
@@ -0,0 +1,116 @@
+#include "types.h"
+#include "riscv.h"
+#include "defs.h"
+#include "param.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "proc.h"
+
+uint64
+sys_exit(void)
+{
+  int n;
+  argint(0, &n);
+  exit(n);
+  return 0;  // not reached
+}
+
+uint64
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+uint64
+sys_fork(void)
+{
+  return fork();
+}
+
+uint64
+sys_wait(void)
+{
+  uint64 p;
+  argaddr(0, &p);
+  return wait(p);
+}
+
+uint64
+sys_sbrk(void)
+{
+  uint64 addr;
+  int n;
+
+  argint(0, &n);
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+uint64
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  argint(0, &n);
+  if(n < 0)
+    n = 0;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(killed(myproc())){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+uint64
+sys_kill(void)
+{
+  int pid;
+
+  argint(0, &pid);
+  return kill(pid);
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+uint64
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+uint64
+ sys_hello(void) {
+    printf("Hello world\n");
+    return 12;
+ }
+
+uint64
+sys_signal(void) {
+    int signum;
+    uint64 handler;
+    // printf("\nIn sys_signal function\n");
+    // Retrieve the signal number and the handler address
+    argint(0, &signum);
+    argaddr(1, &handler);
+    // Only handle SIGINT for now
+    if (signum != SIGINT)
+        return -1;
+    // Set the signal handler for the process
+    struct proc *p = myproc();
+    p->sig_handlers[SIGINT] = (void (*)(int))handler;
+    return 0;
+}
